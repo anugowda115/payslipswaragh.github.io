7f391e2fe961d018919a3bb33f758174d15905c6
anugowda115 <86586526+anugowda115@users.noreply.github.com>
2021-08-31 13:29:57 +0530

    swaragh

---
+++ home.php
@@ -0,0 +1,35 @@
+<?php include 'db_connect.php' ?>
+<style>
+   <!-- Nav Item - HOME -->
+<li class="nav-item active">
+  <a class="nav-link" href="home.php">
+    <i class="fas fa-home"></i>
+    <span>Home</span></a>
+</li>
+
+</style>
+
+<div class="containe-fluid">
+
+	<div class="row">
+		<div class="col-lg-12">
+			
+		</div>
+	</div>
+
+	<div class="row mt-3 ml-3 mr-3">
+			<div class="col-lg-12">
+                <div class="card">
+                    <div class="card-body">
+                    <?php echo "Welcome Back!"  ?>
+                                        
+                    </div>
+                    
+                </div>
+            </div>
+	</div>
+
+</div>
+<script>
+	
+</script>
---
+++ topbar.php
@@ -0,0 +1,25 @@
+<style>
+	
+</style>
+
+<nav class="navbar navbar-light fixed-top bg-primary" style="padding:0;min-height:3.5rem">
+  <div class="container-fluid mt-2 mb-2">
+  	<div class="col-lg-12">
+  		<div class="col-md-1 float-left" style="display: flex;">
+  		
+  		</div>
+
+      <div class="sidebar-brand-icon rotate-n-15 text-white">
+    <i class="fas fa-laugh-wink"></i>
+  </div>
+  
+      <div class="col-md-4 float-left text-white">
+        <large><b>SWARAGH TEC</b></large>
+      </div>
+	  	<div class="col-md-2 float-right text-white">
+	  		<a href="ajax.php?action=logout" class="text-white"><?php echo $_SESSION['login_name'] ?> <i class="fa fa-power-off"></i></a>
+	    </div>
+    </div>
+  </div>
+  
+</nav>
